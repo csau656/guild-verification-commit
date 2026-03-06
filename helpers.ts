707ff7e7374e114efe660d62069e7fa093f2d626
csau656 <140805817+csau656@users.noreply.github.com>
2026-03-07 05:27:50 +0800

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:27:33.419Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T21:27:48.908Z
+Commit: Resolve null pointer exception in user service
