b71c2884896316e7b7d5812488c5b625e61ecdbd
csau656 <140805817+csau656@users.noreply.github.com>
2026-03-07 05:22:45 +0800

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:21:32.004Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T21:22:43.344Z
+Commit: Resolve null pointer exception in user service
